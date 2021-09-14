24fd5528a9966e203dd34d470a3fa85c81ed0a4d
Junior de Oliveira <ellismarjr@hotmail.com>
2021-09-14 13:39:37 -0300

    feat: get statement by client cpf

--- src/index.js
+++ src/index.js
@@ -30,6 +30,14 @@ app.post("/accounts", (request, response) => {
   return response.status(201).send();
 });
 
+app.get("/statements/:cpf", (request, response) => {
+  const { cpf } = request.params;
+
+  const customer = customers.find(customer => customer.cpf === cpf);
+
+  return response.json(customer.statement);
+});
+
 app.listen(3333, () => {
   console.log('Server started on port 3333, 🚀');
 });
